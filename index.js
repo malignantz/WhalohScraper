7fac9968dc9a3ae8100c9719abfcd47cb63b4f64
Garrett Holmes <Garrett.Holmes@gmail.com>
2018-05-14 12:52:13 -0500

    started csv file export

--- index.js
+++ index.js
@@ -8,10 +8,44 @@ var sleep = require("sleep");
 var fs = require("fs");
 
 let app = express();
+
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-// init memory db
+// *********************************
+//        UTILS                   |******************************************************************
+// *********************************
+
+function flatMap(arr) {
+  return arr.reduce((flatArr, item) => flatArr.concat(item), []);
+}
+
+function trimBoth(s) {
+  if (typeof s !== "string") {
+    //  console.error("25_Illegal trim on S. S is a ", typeof s);
+    return s;
+  }
+  s = s.trim();
+  s = s.trimLeft();
+  return s;
+}
+function get$FromURL(url) {
+  return axios.get(url).then(data => {
+    //console.log("130" + JSON.stringify(data.data));
+    var x = cheerio.load(data.data);
+    return x;
+  });
+}
+function throttleCall(fnArray, ms, cb) {
+  let x = setInterval(() => {
+    if (fnArray.length === 0) {
+      clearInterval(x);
+      cb.end("Finished Import.");
+    } else {
+      fnArray.pop()();
+    }
+  }, ms);
+}
 
 const DB = {
   figures: [
@@ -19,6 +53,10 @@ const DB = {
   ],
   plates: {}
 };
+const URLS = { figures: "https://www.serebii.net/duel/figures.shtml" };
+// *********************************
+//        FIGURES                   |******************************************************************
+// *********************************
 
 function getFigure(id) {
   //console.log(typeof id === typeof DB.figures[0].id);
@@ -30,7 +68,7 @@ function setFigure(figure) {
   //  console.log("30" + JSON.stringify(figure));
   Object.assign(DB.figures.find(fig => fig.id === figure.id), figure);
 }
-
+/*
 function addFigure(opts) {
   const {
     id,
@@ -40,9 +78,9 @@ function addFigure(opts) {
     secondaryType,
     attacks,
     movement,
-    rarity,
-    version,
-    image
+    rarity
+    //version,
+    //image
   } = opts;
 
   DB.figures.push({
@@ -53,43 +91,12 @@ function addFigure(opts) {
     secondaryType,
     attacks,
     movement,
-    rarity,
-    version,
-    image
+    rarity
+    // version,
+    //image
   });
 }
-
-// end init mem db
-
-// CONFIG
-
-// helper fns
-function flatMap(arr) {
-  return arr.reduce((flatArr, item) => flatArr.concat(item), []);
-}
-
-function trimBoth(s) {
-  s = s.trim();
-  s = s.trimLeft();
-  return s;
-}
-
-function urlizeName(name) {
-  if (name === undefined) {
-    console.error("Sad face");
-  } else {
-    return name
-      .toLowerCase()
-      .split(" ")
-      .join("-");
-  }
-}
-
-// ======================================
-// Begin Scrapin'
-// ======================================
-const URLS = { figures: "https://www.serebii.net/duel/figures.shtml" };
-
+*/
 function getListOfFigures() {
   var figures = [];
   let addFig = figOptions => {
@@ -135,51 +142,35 @@ function figureIdToURL(id) {
   return result;
 }
 
-function get$FromURL(url) {
-  console.log("128url", url);
-  return axios.get(url).then(data => {
-    //console.log("130" + JSON.stringify(data.data));
-    var x = cheerio.load(data.data);
-    return x;
-  });
-}
-
 function scrapeById(id) {
   let url = figureIdToURL(id);
   return get$FromURL(url)
     .then($ => {
       let figure = parseFigure($);
+      console.log("Scraping ID:", figure.id);
       setFigure(trimFigure(figure));
       return figure;
     })
     .then(x => {
-      console.log("Imported " + x.name);
+      //console.log("154_Imported " + JSON.stringify(x));
     });
 }
 
 function trimFigure(fig) {
   let result = Object.assign({}, fig);
   for (var key in result) {
+    //    console.log(key, result);
+    if (key !== "attacks") {
+    }
     result[key] = trimBoth(result[key]);
   }
   return result;
 }
 
-function throttleCall(fnArray, ms, cb) {
-  let x = setInterval(() => {
-    if (fnArray.length === 0) {
-      clearInterval(x);
-      cb.end("Finished Import.");
-    } else {
-      fnArray.pop()();
-    }
-  }, ms);
-}
-
 function scrapeAllFigures(cb) {
+  console.log("168-scrapeAllFigures");
   let promArray = [];
   let fetchArray = [];
-  let count = 50;
 
   let ids = DB.figures.map(fig => fig.id);
   for (var id of ids) {
@@ -192,6 +183,7 @@ function scrapeAllFigures(cb) {
 }
 
 function parseFigure($) {
+  // console.log("184-parseFigure");
   const RARE_TYPES = {
     ux: true,
     uc: true,
@@ -228,6 +220,7 @@ function parseFigure($) {
     .text()
     .split("\n");
 
+  // this is movement, rarity, type, special ability
   let stats = statsPara.reduce((stats, desc) => {
     if (desc && desc.includes(":")) {
       let [stat, val] = desc.split(":");
@@ -264,9 +257,80 @@ function parseFigure($) {
     return { name, id, ...stats };
   }, {});
 
-  // version && image still needed
-  //console.log("219", stats);
-  return stats;
+  //  console.log("256" + JSON.stringify(stats));
+  // trick out stats and then return it
+
+  var test = $("#moves  table.dextable tr").text();
+  /*
+  console.log(
+    test.length,
+    typeof test,
+    test
+      .split("\n")
+      .map(item => item.trim().trimLeft())
+      .slice(8)
+  );
+  */
+  //console.log("272");
+  let moves = test
+    .split("\n")
+    .map(item => item.trim().trimLeft())
+    .slice(8);
+  let moveArray = [];
+  for (var i = 0; i < moves.length - 5; i++) {
+    let windowData = moves.slice(i, i + 5);
+    //console.log("280wData-", windowData);
+    if (isFinite(parseInt(windowData[0])) && !!windowData[1]) {
+      let data = windowData;
+
+      // console.log(name, windowData);
+      //let [wheelsize, name, color, notes, damage] = data;
+      //console.log("window");
+      // console.log(data);
+      moveArray.push({
+        wheelsize: data[0],
+        name: data[1],
+        color: data[2],
+        notes: data[3],
+        damage: data[4]
+      });
+      // console.log("290" + JSON.stringify(stats));
+    }
+  }
+  /*
+  console.log(
+    "Wheelsize: ",
+    wheelsize,
+    "Name: ",
+    name,
+    "Color: ",
+    color,
+    "Notes: ",
+    notes,
+    "Damage: ",
+    damage
+  );
+*/
+  //console.log("305" + JSON.stringify({ attacks: moveArray, ...stats }));
+  return { attacks: moveArray, ...stats };
+}
+
+function makeCSVData(key) {
+  // doesn't include moves
+
+  let csv =
+    Object.keys(DB[key][0])
+      .filter(x => x !== "attacks")
+      .join(",") + "\n";
+  return DB[key].reduce((csv, item) => {
+    for (var prop in item) {
+      console.log("322-", item);
+      csv += Array.isArray(item[prop])
+        ? item[prop].join(",") + ","
+        : item[prop] + ",";
+    }
+    return csv.slice(0, -1) + "\n";
+  }, "");
 }
 
 function initDb() {
@@ -280,16 +344,6 @@ function initDb() {
     )
   );
 }
-
-// ======================================
-// Begin ExpressJS Server / Endpoints
-// ======================================
-
-// use '/scrape' to scrape all orgs/users and fill database
-// takes about 90s -- once completed, /stats will show summary
-
-// /orgs & /users work only after /scrape
-
 app.get("/", (req, res) => {
   initDb().then(result => {
     const { success, records } = result;
@@ -312,7 +366,7 @@ app.get("/figure/:figureId", (req, res) => {
     var result = getFigure(id);
     //console.log(result);
     if (!error && result.id) {
-      res.end("Name: " + result.name + "_ID: " + result.id);
+      res.end(JSON.stringify(result));
     } else {
       res.end("error");
     }
@@ -332,6 +386,7 @@ app.get("/scrapeAllFigures", (req, res) => {
 app.get("/dump", (req, res) => {
   res.send(JSON.stringify(DB));
   res.end();
+  fs.writeFileSync("figures.csv", makeCSVData("figures"));
 });
 
 app.listen(3000, () => {
